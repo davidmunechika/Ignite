497b006558babbac31b557f25753030866b62910
davidmunechika <davidam1203@gmail.com>
2021-01-06 12:17:50 -0500

    Remove loading delay

--- src/api.js
+++ src/api.js
@@ -37,10 +37,8 @@ export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
 //GAME DETAILS
-export const gameDetailsURL = (game_id) => {
-  `${base_url}games/${game_id}`;
-};
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+
 //GAME SCREENSHOTS
-export const gameScreenshotURL = (game_id) => {
+export const gameScreenshotURL = (game_id) =>
   `${base_url}games/${game_id}/screenshots`;
-};
--- src/components/Game.js
+++ src/components/Game.js
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 //Redux
 import { useDispatch } from 'react-redux';
 import { loadDetail } from '../actions/detailAction';
+import { Link } from 'react-router-dom';
 
 const Game = ({ name, released, image, id }) => {
   //Load Details
@@ -14,15 +15,18 @@ const Game = ({ name, released, image, id }) => {
   };
   return (
     <StyledGame onClick={loadDetailHandler}>
-      <h3>{name}</h3>
-      <p>{released}</p>
-      <img src={image} alt={name} />
+      <Link to={`/game/${id}`}>
+        <h3>{name}</h3>
+        <p>{released}</p>
+        <img src={image} alt={name} />
+      </Link>
     </StyledGame>
   );
 };
 
 const StyledGame = styled(motion.div)`
   min-height: 30vh;
+  cursor: pointer;
   box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
   text-align: center;
   border-radius: 1rem;
